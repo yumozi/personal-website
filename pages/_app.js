270a25bc3486eef6a5dc6ae7bbd9607630b9f990
Yumozi <xueeric1223@gmail.com>
2022-05-19 03:20:15 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,8 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-import Loading from '../components/Loading'
 import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  // const [loading, setLoading] = useState(false);
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 5000)
-  // }, []);
-  // return (
-  //   <div>
-  //     {loading ?
-  //     <Loading/>
-  //     :
-  //     (<Layout>
-  //       <Component {...pageProps} />
-  //     </Layout>)}
-  //   </div>
-  // )
-
   return (
     <div>
       <Layout>
