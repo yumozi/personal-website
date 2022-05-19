fa5072a68a3148cf756fe120a04438af73c63d49
Yumozi <xueeric1223@gmail.com>
2022-05-19 02:49:28 -0400

    Complete description on first paper
    Finished writing the description of the first paper.

--- pages/research.js
+++ pages/research.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Research.module.css'
 import Paper from '../components/Paper'
+import { GoLinkExternal } from 'react-icons/go'
 
 export default function Research() {
   return (
@@ -10,9 +11,13 @@ export default function Research() {
     <hr className={styles.line} />
     <div className={styles.body}>
       <Paper 
-        title="Granular Analysis of Pretrained Detectors"
+        title="Granular Analysis of Pretrained Object Detectors"
         publisher="International Conference on Artificial Intelligence in Information and Communication"
         authors="Eric Xue, Tae Soo Kim"
+        links={[
+          <a href="/papers/GranularAnalysisOfPretrainedObjectDetectors.pdf" target="_blank" className={styles.accent}>PDF <GoLinkExternal/></a>,  
+          <a href="https://ieeexplore.ieee.org/document/9722705/" target="_blank" >IEEE <GoLinkExternal/></a>
+        ]}
       />
     </div>
   </div>
