ee388c685b34e0ece1f055a2d4ac4d818d7a5b75
Yumozi <xueeric1223@gmail.com>
2022-05-19 13:47:44 -0400

    Update aboutme.js
    Changed introduction in aboutme.js

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -9,9 +9,9 @@ export default function AboutMe() {
       </div>
       <div className={styles.body}>
         <div className={styles.paragraphWrapper}>
-          <p>I am a dreamer, but I don&apos;t just dream. </p>
-          <p>I am eager to realize the future that I want to see. A future with much more possibilities to offer than what we have right now.</p>
-          <p>When I discovered coding at a young age, I knew this was the tool that I needed to create the things that I want to create. Since then, I had been building robots of various kinds, different apps, websites, games...</p>
+          <p>There is a certain future that I would like to see.</p>
+          <p>I can&apos;t exactly describe what it looks like, but I will know when we have found it. It is a future that is much more exciting and full of possibilities for everyone.</p>
+          <p>When I discovered programming at a young age, I knew this was the tool that I needed to get us closer to that future. Since then, I had been building robots of various kinds, different apps, websites, games...</p>
           <p>To learn more about the projects that I had done, check out my <Link href="/projects"><a className={styles.accent}>Projects</a></Link> page.</p>
           <p>These days, the majority of my attention is focused on AI. In addition to building AI applications, I also conduct AI-related research. Visit the <Link href="/research"><a className={styles.accent}>Research</a></Link> page to learn more about my published and ongoing research.</p>
           <p>In my personal life, I enjoy a variety of hobbies. Click on <Link href="/personal"><a className={styles.accent}>Personal</a></Link> to find out! </p>
